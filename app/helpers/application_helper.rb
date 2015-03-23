6e6096d301fd06e7ea69e5da7146f3c1ed84653a
Toshinobu Hashizume <toshinobu.hashizume@gmail.com>
2015-03-23 09:55:55 +0900

    201503230100

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+	
+	def full_title(page_title)
+		base_title = "Skeleton Web Site:app/helper/application_helper.rb"
+		if page_title.empty?
+			return base_title
+		else
+			return "#{page_title} | #{base_title}"
+		end
+	end
+
+
 end
