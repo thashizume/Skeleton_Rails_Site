1d215e8059fc6853cbbadb1c595f5581377db392
Toshinobu Hashizume <toshinobu.hashizume@gmail.com>
2015-03-23 17:42:43 +0900

    auto commit 20150323174243

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,26 @@ class SessionsController < ApplicationController
 	def new
 	end
 
+
+
+	#	ログイン処理
+	# @param [:session, :email, :password]
+	# @return render
 	def create
+		user = User.find_by(email: params[:session][:email].downcase)
+		if user && user.authenticate(params[:session][:password])
+			# sign in し、user#show にリダイレクトする				
+					
+					
+						
+		else
+			# login failed. output error message
+			flash[:error] = "invalid email/password combination" 
+			render 'new'			# session#new を renderする
+						
+		end	
+	
+	
 	end
 
 	def destroy
