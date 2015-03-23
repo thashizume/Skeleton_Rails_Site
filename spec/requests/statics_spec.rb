cac8c8f52c072effd635b972ef530fa00f907805
Toshinobu Hashizume <toshinobu.hashizume@gmail.com>
2015-03-23 10:47:50 +0900

    auto commit 20150323104750

--- spec/requests/statics_spec.rb
+++ spec/requests/statics_spec.rb
@@ -1,7 +1,10 @@
 #coding: utf-8
 require 'spec_helper'
 
-describe "静的ページのテストスクリプト" do
+describe "静的ページのテスト" do
+
+	subject {page}
+
 
 #	describe "Home" do
 #		it "should have the content 'Skeleton'" do
@@ -9,6 +12,20 @@ describe "静的ページのテストスクリプト" do
 #			expect(page).to have_content('site')
 #		end
 #	end
+
+	it "link test" do
+		visit root_path 
+		click_link "About US"
+		expect(page).to have_title("About US")
+		click_link "Help"
+		expect(page).to have_title("Help")
+		click_link "Home"
+		click_link "Sign In"
+		expect(page).to have_title("Sign Up")
+
+
+
+	end
 
 
 
